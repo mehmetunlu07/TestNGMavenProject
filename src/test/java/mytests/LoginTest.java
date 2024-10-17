8817df3b120f6ea5be9efcdd849f2f6cf0316b51
mehmetunlu07 <maxbusiness24@gmail.com>
2024-10-17 11:19:41 -0500

    Commit from Intellij

--- src/test/java/mytests/LoginTest.java
+++ src/test/java/mytests/LoginTest.java
@@ -9,12 +9,12 @@ public class LoginTest extends CommonMethods {
 
     @Test
     public void LogoTest() throws InterruptedException {
-        System.out.println("Hello");
+        System.out.println("Hello........................................................");
        // driver.findElement(By.xpath("//textarea[@aria-label='Search']")).sendKeys("news", Keys.RETURN);
         sendTextEnter(google.search,"news");
         Thread.sleep(5000);
 
-        //Assert.assertEquals(driver.findElement(By.xpath("//a[@aria-label='Amazon Business']")).isDisplayed(), true);
+
     }
 
 
